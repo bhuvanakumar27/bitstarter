836d864fe0e53966babc9fb7bafaeb5124ec5857
Steve Thorn <steve.thorn@ed.ac.uk>
2013-07-08 09:38:31 +0000

    Added url argument

--- grader.js
+++ grader.js
@@ -26,6 +26,7 @@ var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "http://infinite-lake-1733.herokuapp.com/";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -55,11 +56,21 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     return out;
 };
 
+var checkURL = function(inurl) {
+    var url = inurl.toString();
+    // could check that the given url is a valid url...
+    return url;
+};
+
 if(require.main == module) {
+    //var url = "test";
     program
         .option('-c, --checks ', 'Path to checks.json', assertFileExists, CHECKSFILE_DEFAULT)
         .option('-f, --file ', 'Path to index.html', assertFileExists, HTMLFILE_DEFAULT)
+        .option('-u, --url ', 'URL', checkURL, URL_DEFAULT)
         .parse(process.argv);
+    if(program.url) {console.log("URL %s", program.url.toString());}
+    if(program.file) {console.log("File %s", program.file.toString());}
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
